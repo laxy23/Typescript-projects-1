5b16a10d488c99530f4736d30cc1db2532aed867
laxy23 <136319798+laxy23@users.noreply.github.com>
2023-06-12 12:28:34 +0200

    Update connect.js

--- my-book-tracker/backend/utils/connect.js
+++ my-book-tracker/backend/utils/connect.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose')
 
 const connect = async () => {
     try {
-        await mongoose.connect(process.env.MONGO_DB)
+        await mongoose.connect("mongodb+srv://laxy23:TzwuBpPk7R2fIZ5B@cluster0.tdowxif.mongodb.net/")
         console.log('Connected to mongoDB')
     } catch (error) {
         throw error
